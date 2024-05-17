59ef53da81dd1bb6f3a16d1be28f318f9146db11
unknown <daniel.sede.garcia19@gmail.com>
2024-05-17 00:46:11 -0600

    Initial commit

---
+++ app/src/main/java/com/example/figura_tu_sueo/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.figura_tu_sueo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.figura_tu_sueo.ui.theme.Figura_tu_sueñoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Figura_tu_sueñoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Figura_tu_sueñoTheme {
+        Greeting("Android")
+    }
+}
